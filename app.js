6d846c06a270034d607578c67d8e6f3828833a4f
Trprsvn <trprsvn@gmail.com>
2019-09-06 01:36:05 +0300

    Seperate indexjs

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,3 @@
 require('dotenv').config()
-
 const { PORT, MONGODB_URI } = process.env
-
 module.exports = { PORT, MONGODB_URI }
