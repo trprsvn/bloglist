0137cb1474979a89b3288ef3e0eb7b37fef9a5ff
Trprsvn <trprsvn@gmail.com>
2019-09-06 01:57:54 +0300

    Complete refactor

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const { MONGODB_URI } = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+const blogsRouter = require('./controllers/blog')
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use('/api/blogs', blogsRouter)
 module.exports = app
